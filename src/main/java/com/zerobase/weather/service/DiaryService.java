dbe9c546fb4764664cf3d4161d5c1ac302e4b68e
jjkim0816 <jjkim0816@gmail.com>
2023-10-28 09:41:16 +0900

    feat: logback 추가

--- src/main/java/com/zerobase/weather/service/DiaryService.java
+++ src/main/java/com/zerobase/weather/service/DiaryService.java
@@ -4,16 +4,20 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.zerobase.weather.ProjectWeatherApplication;
 import com.zerobase.weather.domain.DateWeatherDao;
 import com.zerobase.weather.domain.DiaryDao;
 import com.zerobase.weather.dto.DiaryDto;
+import com.zerobase.weather.exception.WeatherException;
 import com.zerobase.weather.repository.DateWeatherRepository;
 import com.zerobase.weather.repository.DiaryRepository;
+import com.zerobase.weather.type.ErrorCode;
 import com.zerobase.weather.utils.OpenWeather;
 
 import lombok.Getter;
@@ -29,11 +33,18 @@ public class DiaryService {
 	private String region = "Gwangju";
 	@Value("${openweather.key}")
 	private String apiKey;
+	
+	private Logger logger = Logger.getLogger(ProjectWeatherApplication.class);
 
 	@Transactional
 	public List<DiaryDto> findDiary(LocalDate date) {
-		
+		logger.info("start findDiary");
+
 		List<DiaryDao> diaries = diaryRepository.findAllByDate(date);
+		
+		if (diaries.size() == 0) {
+			throw new WeatherException(ErrorCode.WEATHER_DATA_NOT_FOUND);
+		}
 		
 		return diaries.stream()
 				.map(DiaryDto::fromEntity)
@@ -42,7 +53,13 @@ public class DiaryService {
 
 	@Transactional
 	public List<DiaryDto> findDiaries(LocalDate startDate, LocalDate endDate) {
+		logger.info("start findDiaries");
+
 		List <DiaryDao> diaries = diaryRepository.findAllByDateBetween(startDate, endDate);
+		
+		if (diaries.size() == 0) {
+			throw new WeatherException(ErrorCode.WEATHER_DATA_NOT_FOUND);
+		}
 
 		return diaries.stream()
 				.map(DiaryDto::fromEntity)
@@ -51,6 +68,8 @@ public class DiaryService {
 
 	@Transactional
 	public DiaryDto createDiary(LocalDate date, String text) {
+		logger.info("start createDiary");
+
 		DateWeatherDao dateWeatherDao = getDateWeather(date);
 		
 		return DiaryDto.fromEntity(diaryRepository.save(DiaryDao.builder()
@@ -76,6 +95,8 @@ public class DiaryService {
 
 	@Transactional
 	public DiaryDto modifyDiary(LocalDate date, String text) {
+		logger.info("start modifyDiary");
+		
 		DiaryDao diary = diaryRepository.findFirstByDate(date);
 		diary.setText(text);
 		return DiaryDto.fromEntity(diaryRepository.save(diary));
@@ -83,12 +104,16 @@ public class DiaryService {
 
 	@Transactional
 	public Integer deleteDiary(LocalDate date) {
+		logger.info("start deleteDiary");
+
 		return diaryRepository.deleteAllByDate(date);
 	}
 	
 	@Transactional
 	@Scheduled(cron = "0 0 1 * * *")
 	public void saveWeatherDataPerDay() {
+		logger.info("start saveWeatherDataPerDay");
+
 		dateWeatherRepository.save(OpenWeather.getWeatherDateFromApi(region, apiKey));
 	}
 }
