1a2b62c95fe0c4230f15f8863401361c9e8d66a8
jjkim0816 <jjkim0816@gmail.com>
2023-10-29 21:58:16 +0900

    feat: Exception Handler Http 응답 코드 어노테이션 추가

--- src/main/java/com/zerobase/weather/exception/GlobalException.java
+++ src/main/java/com/zerobase/weather/exception/GlobalException.java
@@ -1,6 +1,8 @@
 package com.zerobase.weather.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.zerobase.weather.dto.ErrorResponse;
@@ -8,11 +10,13 @@ import com.zerobase.weather.type.ErrorCode;
 
 @RestControllerAdvice
 public class GlobalException {
+	@ResponseStatus(HttpStatus.OK)
 	@ExceptionHandler(WeatherException.class)
 	public ErrorResponse handlerWeatherException(WeatherException e) {
 		return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
 	}
 	
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
 	public ErrorResponse handlerException(Exception e) {
 		return new ErrorResponse(
